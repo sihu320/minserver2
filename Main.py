c7680c4a0a524ce2c0aee702414188ce04754c8a
sihu320 <123544404+sihu320@users.noreply.github.com>
2023-10-22 10:55:36 +0900

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,9 @@
+import streamlit as st
+st.set_page_config(
+    page_title= 'minecraft map',
+    page_icon= 'img\minecraftemg.png'
+    
+)
+st.title('마크 지도')
+st.markdown('**마크** 서버 지도 및 참고자료?')
+st.image('img\Warwick_Render.png')
---
+++ pages/1_Map.py
@@ -0,0 +1,12 @@
+import streamlit as st
+st.set_page_config(
+    page_title= 'minecraft map',
+    page_icon= 'img\minecraftemg.png'
+)
+lab1 = '측면'
+lab2 = '정면 3층'
+
+with st.expander(label=lab1):
+    st.image('img\sideview.png',use_column_width=True)
+with st.expander(label=lab2):
+    st.image('img\Frontview.png',use_column_width=True)
